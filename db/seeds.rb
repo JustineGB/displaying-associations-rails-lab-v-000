8fe0eea4dc308bfe0c2f7fd272484ee6d321489b
Justine B <justinegayton@gmail.com>
2019-01-03 16:56:39 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+artist1 = Artist.create!(name: "Grateful Dead")
+artist1.songs.create!(title: "Brokedown Palace")
+artist1.songs.create!(title: "Chinacat Sunflower")
+artist1.songs.create!(title: "Jack Straw")
+ 
+artist2 = Artist.create!(name: "Phish")
+artist2.songs.create!(title: "Carini")
